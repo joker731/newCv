41e2861194efb454e9b4d212d2456b1cfff035a5
chenting <972496805@qq.com>
2018-04-10 16:03:02 +0800

    新效果

---
+++ js/init-swiper.js
@@ -0,0 +1,17 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    speed:1000,
+    
+
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    // 如果需要滚动条
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })  
